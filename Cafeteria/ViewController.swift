ca2435250cee19111e8a58f40f3719dcc6ad27c7
Daniel Zoller <nosebrain@gmx.net>
2015-01-25 04:04:56 +0100

    Initial Commit

---
+++ Cafeteria/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Cafeteria
+//
+//  Created by Daniel Zoller on 25.01.15.
+//  Copyright (c) 2015 nosebrain. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
